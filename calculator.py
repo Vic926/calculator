f0b5aaf01f1001693e5a21e2e34422f2c667f893
Victor Waweru <mwauravictor92@mgmail.com>
2019-03-16 00:20:07 +0300

    Change layout

--- calculator.py
+++ calculator.py
@@ -8,8 +8,7 @@ def clr_all():
 
 
 def bck_spc():
-    entry1.delete()
-    return
+    pass
 
 
 def click(n):
@@ -20,7 +19,7 @@ def click(n):
 def equals():
     e = eval(str(entry1.get()))
     entry1.delete(0, 'end')
-    entry1.insert('end', str(e))
+    entry1.insert('end', str(round(e, 8)))
 
 
 
@@ -28,120 +27,130 @@ def equals():
 window = tk.Tk()
 window.title('My Calculator')
 
-entry1 = tk.Entry(bd=8)
-entry1.grid(column=0, row=0)
+
+#---------------------------Frames-----------------------------
+
+frame1 = tk.Frame()
+frame1.pack(side='top')
+frame2 = tk.Frame()
+frame2.pack(side = 'top')
+frame3 = tk.Frame()
+frame3.pack(side='top')
+frame4 = tk.Frame()
+frame4.pack(side='top')
+frame5 = tk.Frame()
+frame5.pack(side='top')
+frame6 = tk.Frame()
+frame6.pack(side='top')
+frame7 = tk.Frame()
+frame7.pack(side='top')
+frame8 = tk.Frame()
+frame8.pack(side='top')
+
+#-------------------entry-field------------------------------------
+entry1 = tk.Entry(frame1, bd=8, width=34)
+entry1.pack(side='left')
+
+#-------------------------Buttons--------------------------------
+button7 = tk.Button(frame2, font=43, text=7, padx=16, pady=16,fg='white', bg='blue', bd=4, command=lambda: click('7'))
+button7.pack(side='left')
+
+button8 = tk.Button(frame2, font=43, text=8, padx=16, pady=16, fg='white', bg='blue', bd=4, command=lambda: click('8'))
+button8.pack(side='left')
+
+button9 = tk.Button(frame2, font=43, text=9, padx=16, pady=16, fg='white', bg='blue',bd=4, command=lambda: click('9'))
+button9.pack(side='left')
+
+
+button4 = tk.Button(frame3, font=43, text=4, padx=16, pady=16, fg='white', bg='blue',bd=4, command=lambda: click('4'))
+button4.pack(side='left')
+
+button5 = tk.Button(frame3, font=43, text=5, padx=16, pady=16, fg='white', bg='blue',bd=4,command=lambda: click('5') )
+button5.pack(side='left')
+
+button6 = tk.Button(frame3, font=43, text=6, padx=16, pady=16, fg='white', bg='blue',bd=4, command=lambda: click('6'))
+button6.pack(side='left')
 
 
 
+button1 = tk.Button(frame4, font=43, text=1, padx=16, pady=16, fg='white', bg='blue',bd=4, command=lambda: click('1') )
+button1.pack(side='left')
 
+button2 = tk.Button(frame4, font=43, text=2, padx=16, pady=16, fg='white', bg='blue',bd=4, command=lambda: click('2'))
+button2.pack(side='left')
 
-button7 = tk.Button( font=43, text=7, padx=16, pady=16,fg='white', bg='blue', bd=4, command=lambda: click('7'))
-button7.grid(column=1, row=0)
-
-button8 = tk.Button(font=43, text=8, padx=16, pady=16, fg='white', bg='blue', bd=4, command=lambda: click('8'))
-button8.grid(column=2, row=0)
-
-button9 = tk.Button(font=43, text=9, padx=16, pady=16, fg='white', bg='blue',bd=4, command=lambda: click('9'))
-button9.grid(column=3, row=0)
-
-
-button4 = tk.Button(font=43, text=4, padx=16, pady=16, fg='white', bg='blue',bd=4, command=lambda: click('4'))
-button4.grid(column=1, row=1)
-
-button5 = tk.Button(font=43, text=5, padx=16, pady=16, fg='white', bg='blue',bd=4,command=lambda: click('5') )
-button5.grid(column=2, row=1)
-
-button6 = tk.Button(font=43, text=6, padx=16, pady=16, fg='white', bg='blue',bd=4, command=lambda: click('6'))
-button6.grid(column=3, row=1)
+button3 = tk.Button(frame4, font=43, text=3, padx=16, pady=16, fg='white', bg='blue',bd=4, command=lambda: click('3'))
+button3.pack(side='left')
 
 
 
-button1 = tk.Button(font=43, text=1, padx=16, pady=16, fg='white', bg='blue',bd=4, command=lambda: click('1') )
-button1.grid(column=1, row=2)
+btn_pls = tk.Button(frame2, font=43, text='+', padx=16, pady=16, fg='black', bg='red',bd=4, width= 8, command=lambda: click('+'))
+btn_pls.pack(side='left')
 
-button2 = tk.Button(font=43, text=2, padx=16, pady=16, fg='white', bg='blue',bd=4, command=lambda: click('2'))
-button2.grid(column=2, row=2)
+btn_subtract = tk.Button(frame3, font=43, text='-', padx=16, pady=16, fg='black', bg='red',bd=4, width=8, command=lambda: click('-'))
+btn_subtract.pack(side='left')
 
-button3 = tk.Button(font=43, text=3, padx=16, pady=16, fg='white', bg='blue',bd=4, command=lambda: click('3'))
-button3.grid(column=3, row=2)
-
-
+btn_times = tk.Button(frame4, font=43, text='X', padx=16, pady=16, fg='black', bg='red',bd=4, width=8, command=lambda: click('*'))
+btn_times.pack(side='left')
 
 
+zer0 = tk.Button(frame5, font=50, text='0', padx=16, pady=16, fg='white', bg='blue',bd=4, width=1,command=lambda: click('0'))
+zer0.pack(side='left')
 
+btn_pwr = tk.Button(frame5, font=43, text='^', padx=14, pady=16, fg='white', bg='blue',bd=4,
+                    command=lambda:click('**'))
+btn_pwr.pack(side='left')
 
-btn_pls = tk.Button(font=43, text='+', padx=16, pady=16, fg='black', bg='red',bd=4, width= 8, command=lambda: click('+'))
-btn_pls.grid(column=0, row=1)
-
-btn_subtract = tk.Button(font=43, text='-', padx=16, pady=16, fg='black', bg='red',bd=4, width=8, command=lambda: click('-'))
-btn_subtract.grid(column=0, row=2)
-
-btn_times = tk.Button(font=43, text='X', padx=16, pady=16, fg='black', bg='red',bd=4, width=8, command=lambda: click('*'))
-btn_times.grid(column=0, row=3)
-
-btn_dvd = tk.Button(font=43, text='/', padx=16, pady=16, fg='black', bg='red',bd=4, width=8, command=lambda: click('/'))
-btn_dvd.grid(column=0, row=4)
-
-
-
-
-zer0 = tk.Button(font=50, text='0', padx=16, pady=16, fg='white', bg='blue',bd=4, width=1,command=lambda: click('0'))
-zer0.grid(column=1, row=3)
-
-btn_pwr = tk.Button(font=43, text='^', padx=14, pady=16, fg='white', bg='blue',bd=4,
-                    command=lambda:click('**') )
-btn_pwr.grid(column=2, row=3)
-
-sqroot = tk.Button(font=43, text='²√', padx=14, pady=16, fg='white', bg='blue', bd=4,
+sqroot = tk.Button(frame5, font=43, text='²√', padx=14, pady=16, fg='white', bg='blue', bd=4,
                    command=lambda:click('math.sqrt('))
-sqroot.grid(column=3, row=3)
+sqroot.pack(side='left')
+
+btn_dvd = tk.Button(frame5, font=43, text='/', padx=16, pady=16, fg='black', bg='red',bd=4, width=8, command=lambda: click('/'))
+btn_dvd.pack(side='left')
 
 
+brckt1 = tk.Button(frame6, font=40, text='(', padx=18, pady=16, fg='white', bg='blue',bd=4, command=lambda: click('('))
+brckt1.pack(side='left')
 
-sine = tk.Button(font=40, text='sin', padx=10, pady=10, fg='white', bg='blue',bd=4,
-                 command=lambda:click('math.sin('))
-sine.grid(column=1, row=4)
+brckt2 = tk.Button(frame6, font=40, text=')', padx=18, pady=16, fg='white', bg='blue', bd=4, command=lambda: click(')'))
+brckt2.pack(side='left')
 
-cosine = tk.Button(font=40, text='cos', padx=10, pady=10, fg='white', bg='blue',bd=4,
-                   command=lambda:click('math.cos('))
-cosine.grid(column=2, row=4)
-
-tan = tk.Button(font=40, text='tan', padx=10, pady=10, fg='white', bg='blue',bd=4,
-                command=lambda:click('math.tan('))
-tan.grid(column=3, row=4)
-
-
-brckt1 = tk.Button(font=40, text='(', padx=16, pady=9, fg='white', bg='blue',bd=4, command=lambda: click('('))
-brckt1.grid(column=1, row=5)
-
-brckt2 = tk.Button(font=40, text=')', padx=16, pady=9, fg='white', bg='blue',bd=4, command=lambda: click(')'))
-brckt2.grid(column=2, row=5)
-
-dot = tk.Button(font=40, text='.', padx=16, pady=9, fg='white', bg='blue',bd=4,
+dot = tk.Button(frame6, font=40, text='.', padx=18, pady=16, fg='white', bg='blue', bd=4,
                 command=lambda: click('.'))
-dot.grid(column=3, row=5)
+dot.pack(side='left')
 
-pi = tk.Button(font=40, text='π', padx=16, pady=9, fg='white', bg='blue',bd=4,width=8, command=lambda: click('math.pi'))
-pi.grid(column=0, row=5)
-
-
+pi = tk.Button(frame6, font=40, text='π', padx=16, pady=16, fg='white', bg='blue',bd=4,width=8, command=lambda: click('math.pi'))
+pi.pack(side='left')
 
 
+sine = tk.Button(frame7, font=40, text='sin', padx=10, pady=10, fg='white', bg='purple',bd=4,
+                 command=lambda:click('math.sin('))
+sine.pack(side='left')
 
-eqls = tk.Button(font=43, text='=', padx=16, pady=10, fg='white', bg='blue',bd=4, width=8, command=equals)
-eqls.grid(column=0, row=6)
+cosine = tk.Button(frame7, font=40, text='cos', padx=10, pady=10, fg='white', bg='purple',bd=4,
+                   command=lambda:click('math.cos('))
+cosine.pack(side='left')
 
-log = tk.Button(font=43, text='log', padx=10, pady=10, fg='white', bg='blue',bd=4,
+tan = tk.Button(frame7, font=40, text='tan', padx=10, pady=10, fg='white', bg='purple',bd=4,
+                command=lambda:click('math.tan('))
+tan.pack(side='left')
+
+log = tk.Button(frame7, font=43, text='log', padx=10, pady=10, fg='white', bg='blue', bd=4,
                 command=lambda: click('math.log('))
-log.grid(column=1, row=6)
+log.pack(side='left')
 
-bck_spce = tk.Button(font=43, text='C', padx=16, pady=10, fg='white', bg='blue',bd=4, command=bck_spc)
-bck_spce.grid(column=2, row=6)
+factorial = tk.Button(frame7, font=43, text='!', padx=19, pady=10, fg='white', bg='blue', bd=4,
+                     command=lambda: click('math.factorial('))
+factorial.pack(side='left')
 
-delete = tk.Button(font=40, text='CE', padx=10, pady=10, fg='white', bg='blue',bd=4, command=clr_all)
-delete.grid(column=3, row=6)
+bck_spce = tk.Button(frame8, font=43, text='C', padx=16, pady=16, fg='white', bg='blue',bd=4, command=bck_spc)
+bck_spce.pack(side='left')
 
+delete = tk.Button(frame8, font=40, text='CE', padx=11, pady=16, fg='white', bg='blue',bd=4, command=clr_all)
+delete.pack(side='left')
 
+eqls = tk.Button(frame8, font=43, text='=', padx=24, pady=16, fg='white', bg='black',bd=4, width=11,command=equals)
+eqls.pack(side='left')
 
 
 window.mainloop()
